bda4ed8d8d0a2ef640bb9c4c8c8e4f009ea851d3
Антон Красноперов <an2niy85@mail.ru>
2021-11-23 00:02:49 +0400

    Добавьте файлы проекта.

---
+++ KansasCityShuffle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KansasCityShuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            Shuffle(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+            
+        }
+
+        public static void Shuffle(int[] array)
+        {
+            if (array.Length < 1) return;
+            Random random = new Random();
+            for (var i = 0; i < array.Length; i++)
+            {
+                int key = array[i];
+                int rnd = random.Next(i, array.Length);
+                array[i] = array[rnd];
+                array[rnd] = key;
+            }
+        }
+    }
+}
